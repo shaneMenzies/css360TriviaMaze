b317cb9734f781d882d249610a24c6500563cd34
Arafa Mohamed <arafam@uw.edu>
2024-10-25 20:35:28 -0700

    I have created a Player class and implemented most of it

---
+++ src/main/model/Player.java
@@ -0,0 +1,115 @@
+package model;
+
+// STILL WORKING ON IT, BUT ALMOST DONE.
+
+/**
+ * Represents a player in the game, tracking their position, score, and lives.
+ *
+ * @author Arafa Mohamed
+ * @version 10/25/24
+ */
+public class Player {
+    private Coordinates myPosition;
+    private int myScore;
+    private int myLives;
+    private List<PlayerUpdateListener> myListeners;
+
+    /**
+     * Constructs a new Player with the specified position, score, and number of lives.
+     * @param position the starting coordinates of the player
+     * @param score    the initial score of the player
+     * @param lives    the initial number of lives of the player
+     */
+    public Player(Coordinates position, int score, int lives) {
+        myPosition = position;
+        myScore = score;
+        myLives = lives;
+    }
+
+    /**
+     * Notifies all registered listeners about player updates.
+     */
+    private void updateListeners() {
+
+    }
+
+    /**
+     * Gets the current position of the player.
+     * @return the current coordinates of the player
+     */
+    public Coordinates getPosition() {
+        return myPosition;
+    }
+
+    /**
+     * Gets the current score of the player.
+     * @return the current score of the player
+     */
+    public int getScore() {
+        return myScore;
+    }
+
+    /**
+     * Gets the current number of lives of the player.
+     * @return the current number of lives of the player
+     */
+    public int getLives() {
+        return myLives;
+    }
+
+    /**
+     * Sets the player's position and updates listeners.
+     * @param position the new coordinates for the player
+     */
+    public void setPosition(Coordinates position) {
+        myPosition = position;
+        updateListeners();
+    }
+
+    /**
+     * Sets the player's score and updates listeners.
+     * @param score the new score for the player
+     */
+    public void setScore(int score) {
+        myScore = score;
+        updateListeners();
+    }
+
+    /**
+     * Sets the player's number of lives and updates listeners.
+     * @param lives the new number of lives for the player
+     */
+    public void setLives(int lives) {
+        myLives = lives;
+        updateListeners();
+    }
+
+    /**
+     * Adds a listener that will be notified of updates to this player.
+     * @param listener the listener to be added
+     */
+    public void addUpdateListener(PlayerUpdateListener listener) {
+        myListeners.add(listener);
+    }
+
+    /**
+     * Removes a listener so that it will no longer receive updates.
+     * @param listener the listener to be removed
+     */
+    public void removeUpdateListener(PlayerUpdateListener listener) {
+        myListeners.remove(listener);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
