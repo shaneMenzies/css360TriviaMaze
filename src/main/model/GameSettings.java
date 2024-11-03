02c9a41f707a75dda688b75a0efc616c38320d74
Shane Menzies <menzies.r.shane@gmail.com>
2024-11-03 11:41:56 -0800

    GameSettings class implemented with Unit Tests

---
+++ src/main/model/GameSettings.java
@@ -0,0 +1,70 @@
+package model;
+
+/**
+ * Holds the settings for an active game.
+ *
+ * @author Shane Menzies
+ * @version 11/3/24
+ */
+public final class GameSettings {
+
+    /**
+     * Setting for how many lives the player starts with.
+     */
+    private final int myInitialPlayerLives;
+
+    /**
+     * Setting for how the player's score changes after a question
+     * is answered correctly.
+     */
+    private final int myCorrectAnswerScore;
+
+    /**
+     * Setting for how the player's score changes after a question
+     * is answered incorrectly.
+     */
+    private final int myWrongAnswerScore;
+
+    /**
+     * Makes a new GameSettings with the provided settings.
+     *
+     * @param theInitialLives How many lives the player starts with.
+     * @param theCorrectScore Change to player's score on a correct answer.
+     * @param theWrongScore Change to player's score on an incorrect answer.
+     */
+    public GameSettings(final int theInitialLives,
+                        final int theCorrectScore, final int theWrongScore) {
+        myInitialPlayerLives = theInitialLives;
+        myCorrectAnswerScore = theCorrectScore;
+        myWrongAnswerScore = theWrongScore;
+    }
+
+    /**
+     * Gets how many lives the player should start with.
+     *
+     * @return Number of lives the player should start with.
+     */
+    public int getInitialPlayerLives() {
+        return myInitialPlayerLives;
+    }
+
+    /**
+     * Gets the value which should be added to the player's score
+     * when they answer a question correctly.
+     *
+     * @return Value to be added to score on a correct answer.
+     */
+    public int getCorrectAnswerScore() {
+        return myCorrectAnswerScore;
+    }
+
+    /**
+     * Gets the value which should be added to the player's score
+     * when they answer a question incorrectly.
+     *
+     * @return Value to be added to score on an incorrect answer.
+     */
+    public int getWrongAnswerScore() {
+        return myWrongAnswerScore;
+    }
+}
